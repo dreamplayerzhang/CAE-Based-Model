f33cd3db9d99f75006bc701d385ffbb51a6a23a7
FXiaoRen <awesome_rfx@163.com>
2018-04-07 21:56:41 +0800

    prepare dataset to .csv file； design the CAE Model.

--- networks.py
+++ networks.py
@@ -1,68 +1,96 @@
-import torch as t
+import torch
 from torch.autograd import Variable
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
 import numpy as np
+import pandas as pd
+
+dtype = torch.FloatTensor
+batch_size = 4
+
 
 class Net(nn.Module):
     def __init__(self):
         nn.Module.__init__(self)
-        self.conv1 = nn.Conv2d(1, 6, 5)
-        self.conv2 = nn.Conv2d(6, 16, 5)
-        self.fc1 = nn.Linear(5*5*16, 120)
-        self.fc2 = nn.Linear(120, 84)
-        self.fc3 = nn.Linear(84, 10)
+        self.encoder = nn.Sequential(
+            nn.Conv2d(1, 8, 3, padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d((2, 2)),
+            nn.Conv2d(8, 16, 3, padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d((2, 2)),
+            nn.Conv2d(16, 32, 1, padding=0),
+            nn.ReLU(),
+            # nn.MaxPool2d((2, 2)),
+            nn.Conv2d(32, 64, 1, padding=0),
+            nn.ReLU(),
+            nn.Conv2d(64, 128, 1, padding=0),
+            nn.ReLU(),
+            nn.Conv2d(128, 256, 1, padding=0),
+            nn.ReLU(),
+        )
+
+        self.decoder = nn.Sequential(
+            nn.Conv2d(256, 128, 1, padding=0),
+            nn.ReLU(),
+            nn.Conv2d(128, 64, 1, padding=0),
+            nn.ReLU(),
+            nn.Conv2d(64, 32, 1, padding=0),
+            nn.ReLU(),
+            # nn.UpsamplingBilinear2d(scale_factor=2),
+            nn.Conv2d(32, 16, 1, padding=0),
+            nn.ReLU(),
+            nn.UpsamplingNearest2d(scale_factor=2),
+            nn.Conv2d(16, 8, 3, padding=1),
+            nn.ReLU(),
+            nn.UpsamplingNearest2d(scale_factor=2),
+            nn.Conv2d(8, 1, 3, padding=1),
+            nn.ReLU(),
+        )
 
     def forward(self, x):
-        x = F.max_pool2d(F.relu(self.conv1(x)), (2, 2))
-        x = F.max_pool2d(F.relu(self.conv2(x)), 2)
-        x = x.view(x.size()[0], -1)
-        x = F.relu(self.fc1(x))
-        x = F.relu(self.fc2(x))
-        x = self.fc3(x)
-        return x
+        encoded = self.encoder(x)
+        decoded = self.decoder(encoded)
+        return decoded
 
 net = Net()
-# print(net)
-
-# for name, params in net.named_parameters():
-#     print(name, ':', params.size())
-
-
-input = Variable(t.rand(1, 1, 32, 32))
-out = net(input)
-# print(out.size())
-
-net.zero_grad()
-# out.backward(Variable(t.ones(1, 10)))
-# print(net.conv1.bias.grad)
-
-target = Variable(t.arange(0, 10))
 criterion = nn.MSELoss()
-loss = criterion(out, target)
+optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
 
-# print(input)
-print(out)
-# print(target)
-print(loss)
+data = pd.read_csv('./dataset/patches_csv/8-4.csv')
+data.drop(['Unnamed: 0'], axis=1, inplace=True)
+data = data.sample(frac=0.1)
+data = data.values.reshape([-1, 1, 8, 8])
+batches = int(data.shape[0])
+# batches = int(data.shape[0] / batch_size)
 
-# print("before bp:")
-# print(net.conv1.bias.grad)
-# loss.backward()
-#
-# print("after bp:")
-# print(net.conv1.bias.grad)
+for epoch in range(2):
+    running_loss = 0.0
+    for i in range(batches):
+        inputs = data[i].reshape([1, 1, 8, 8])
+        # inputs = data[i:i + batch_size, :, :, :]
+        inputs = torch.from_numpy(inputs).float()
+        inputs = Variable(inputs)
 
-optimizer = optim.SGD(net.parameters(), lr=0.01)
-optimizer.zero_grad()
+        optimizer.zero_grad()
 
-loss.backward()
-optimizer.step()
+        outputs = net(inputs)
+
+        loss = criterion(outputs, inputs)
+        loss.backward()
+
+        optimizer.step()
+
+        running_loss += loss.data[0]
+        if i == 0:
+            print('[%d, %5d] loss: %.6f' % (epoch + 1, i + 1, running_loss))
+        if i % 500 == 499:
+            print('[%d, %5d] loss: %.6f' % (epoch + 1, i + 1, running_loss / 500))
+            # print(inputs)
+            # print(outputs)
+            torch.save(net.state_dict(), 'CAE_Nearest2d.pth')
+            running_loss = 0.0
 
 
-print('========================')
-out = net(input)
-loss = criterion(out, target)
-print(out)
-print(loss)
+
